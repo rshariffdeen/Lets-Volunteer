7945d9b9556dc7f33e77289ec43247d261acb76b
Ridwan Shariffdeen <ridwan.11@cse.mrt.ac.lk>
2014-05-09 11:27:32 -0700

    Basic Template Updated

--- src/Spiralation/UserBundle/Controller/HomeController.php
+++ src/Spiralation/UserBundle/Controller/HomeController.php
@@ -9,12 +9,12 @@ class HomeController extends Controller
     public function organizationAction(Request $request)
     {
         
-        return $this->render('SpiralationUserBundle:Profile:volunteer.html.twig');
+        return $this->render('SpiralationUserBundle:Profile:volunteer.html.twig', array('id'=>$request->getSession()->get('org_id')));
     }
     
     public function volunteerAction(Request $request)
     {
         
-        return $this->render('SpiralationUserBundle:Profile:volunteer.html.twig');
+        return $this->render('SpiralationUserBundle:Profile:volunteer.html.twig', array('id'=>$request->getSession()->get('vol_id')));
     }
 }

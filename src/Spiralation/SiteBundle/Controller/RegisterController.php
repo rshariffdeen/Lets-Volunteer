7fac2b49205bc3a9a308d7b0f10b919c6a0f6888
Ridwan Shariffdeen <ridwan.11@cse.mrt.ac.lk>
2014-05-08 21:18:30 -0700

    Organization Registration Form

--- src/Spiralation/SiteBundle/Controller/RegisterController.php
+++ src/Spiralation/SiteBundle/Controller/RegisterController.php
@@ -7,20 +7,22 @@ use Spiralation\EntityBundle\Entity\Volunteer;
 use Spiralation\EntityBundle\Form\VolunteerType;
 use Spiralation\EntityBundle\Entity\User;
 
+use Spiralation\EntityBundle\Entity\Organization;
+use Spiralation\EntityBundle\Form\OrganizationType;
 
 class RegisterController extends Controller
 {
-    public function formAction(Request $request)
+    public function VolunteerAction(Request $request)
     {
-       $Volunteer = new Volunteer();
-            $form = $this->createForm(new VolunteerType(), $Volunteer, array(
+       $volunteer = new Volunteer();
+            $form = $this->createForm(new VolunteerType(), $volunteer, array(
                 'action' => $this->generateUrl('create_volunteer'),
                 'attr'  => array(
                     'class'=>'form-horizontal center'
                 )
             ));
             
-        $volunteer = new Volunteer();   
+      
         $form->handleRequest($request);
 
         if ($form->isValid()) {
@@ -50,6 +52,54 @@ class RegisterController extends Controller
             return $this->render('SpiralationSiteBundle:index:index.html.twig', array(
                     'type'  => 'S',
                     'message' => ' Congratulations! You are now an Volunteer. You can now sign in'
+                    
+        ));
+        }
+          
+           return $this->render('SpiralationSiteBundle:Register:register.html.twig', array('form' => $form->createView())); 
+        
+    }
+    
+     public function OrganizationAction(Request $request)
+    {
+       $Organization = new Organization();
+            $form = $this->createForm(new OrganizationType(), $Organization, array(
+                'action' => $this->generateUrl('create_organization'),
+                'attr'  => array(
+                    'class'=>'form-horizontal center'
+                )
+            ));
+            
+       
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $Organization = $form->getData();
+            $user = new User();
+            $user->setPassword($Organization->getPassword());
+            $user->setRole(2);
+            $user->setOrganization($Organization->getOrganizationId());
+            $user->setEmail($Organization->getEmail());
+        
+        try{
+            $em = $this->getDoctrine()->getManager();
+            $em ->persist($Organization);
+            $em->persist($user);            
+            $em->flush();
+            }
+            catch(\Exception $e){
+                echo $e;
+                return $this->render('SpiralationSiteBundle:Index:index.html.twig', array(
+                    'type'  => 'E',
+                    'message' => ' Opz! Something went wrong. Please Try again'
+                    
+                    
+        ));
+            }
+
+            return $this->render('SpiralationSiteBundle:index:index.html.twig', array(
+                    'type'  => 'S',
+                    'message' => ' Congratulations! Your Organization is added. You can now sign in'
                     
         ));
         }

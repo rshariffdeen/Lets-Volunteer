55e58101eee25c5e6620907a93d06274c718d9bb
Ridwan Shariffdeen <ridwan.11@cse.mrt.ac.lk>
2014-05-09 10:53:39 -0700

    Home Page Editing

--- src/Spiralation/UserBundle/Controller/HomeController.php
+++ src/Spiralation/UserBundle/Controller/HomeController.php
@@ -4,9 +4,15 @@ namespace Spiralation\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-class ProfileController extends Controller
+class HomeController extends Controller
 {
-    public function homeAction(Request $request)
+    public function organizationAction(Request $request)
+    {
+        
+        return $this->render('SpiralationUserBundle:Profile:volunteer.html.twig');
+    }
+    
+    public function volunteerAction(Request $request)
     {
         
         return $this->render('SpiralationUserBundle:Profile:volunteer.html.twig');
